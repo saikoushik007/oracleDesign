755a9f23f8dbb4b797db63395ff01869aa46ac91
SaiKoushik Chavali <chavalis@vmware.com>
2021-04-05 10:24:33 +0530

    Oracle Designs

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit } from '@angular/core';
+import { TileService } from '../shared/services/tile.service';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+import filters from '../shared/filters.json';
+import { TagInputModule } from 'ngx-chips';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+TagInputModule.withDefaults({
+  tagInput: {
+    placeholder: '',
+    hideForm: true
+  }
+});
+
+
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css'],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ height: 0, opacity: 0 }),
+            animate('1s ease-out',
+              style({ height: 300, opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({ height: 300, opacity: 1 }),
+            animate('1s ease-in',
+              style({ height: 0, opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
+})
+export class MainComponent implements OnInit {
+
+  tilesData: any = [];
+  displayedItems: number = 0;
+  displayTiles: any = [];
+  disableLoad: boolean = false;
+  filterObject: any = new Array();
+  regionArray: any = filters.region;
+  lobArray: any = filters.lob;
+  assetTypeArray: any = filters.type;
+  filteredData: any = [];
+  showFilter: boolean = false;
+
+  constructor(private tileService: TileService) {
+  }
+
+  ngOnInit(): void {
+    this.getTilesData();
+  }
+
+  getTilesData() {
+    this.tileService.getTilesData().subscribe(data => {
+      this.tilesData = data;
+      this.displayRecords();
+    });
+  }
+
+  displayRecords(): void {
+    var data = (this.filterObject.length > 0) ? this.filteredData : this.tilesData;
+    var i = 0;
+    do {
+      this.displayTiles.push(data[this.displayedItems]);
+      i++;
+      this.displayedItems++;
+    } while (i != 10 && this.displayedItems != data.length);
+    this.disableLoad = (this.displayedItems >= data.length) ? true : false;
+  }
+
+  displayFilteredRecords(): void {
+    this.filteredData = [];
+    for (var i = 0; i < this.filterObject.length; i++) {
+      var filter = this.filterObject[i];
+      this.filteredData.push(...this.tilesData.filter(function (el) { return el[filter.module].includes(filter.value) }));//spread operator
+    }
+    this.displayTiles = [];
+    this.displayedItems = 0;
+    this.displayRecords();
+  }
+  upvote(item): void {
+    item.upvote += 1;
+    this.tileService.upVote(item).subscribe(data => {
+    });
+  }
+  onRemove(item) {
+    console.log('Modules', filters[item.module]);
+    filters[item.module][item.index]['selected'] = false;
+    return of(item);
+  }
+
+  checkBoxSelect(event) {
+    var itemChecked = event.source.__ngContext__[8].checkBoxLabel;
+    console.log('Item Checked', itemChecked);
+
+    if (event.checked) {
+      this.filterObject.push(itemChecked);
+    }
+
+    else {
+      this.filterObject = this.filterObject.length > 0 ? this.filterObject.filter(function (el) { return el.value != itemChecked.value; }) : [];
+    }
+  }
+  showFilters() {
+    localStorage.topper = document.body.scrollTop;
+    window.scrollTo(0, localStorage.topper + 250);
+    this.showFilter = !this.showFilter;
+  }
+}
